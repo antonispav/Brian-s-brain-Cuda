eccc1d06a62dcc6bbe38d37f38ec44c666ddd123
bewater <bewatermf@cslab.unipi.gr>
2020-02-24 17:41:49 +0200

    Device Code-Shared Memory

--- Brian.v1.h
+++ Brian.v1.h
@@ -4,13 +4,13 @@ __global__ void play_with_shared_memory(int *in, int *out, int size)
 	int bid = blockIdx.x;
 	int tid = threadIdx.x;
 	int bdim = blockDim.x;
-	int max = size * size;//to megethos tou pinaka
+	int max = size * size;//matrix size
 	int i = bid * bdim + tid;//block i
 	int mod = i % size;
 
-	extern __shared__ int local_board[];//pinakas pou vriskete sthn sharedmem kai to megethos tou dinete sthn ekinisi tou programm
+	extern __shared__ int local_board[];//matrix located in sharedmem and its size is given at the start of the programm
 
-	int live_cells = 0;//metritis live 
+	int live_cells = 0;//live counter
 
 	local_board[tid + size] = in[i];
 
@@ -39,42 +39,42 @@ __global__ void play_with_shared_memory(int *in, int *out, int size)
 	}
 	if (mod != size - 1 && i + size < max && local_board[lid + size + 1])// Top right
 	{
-		live_cells++;	
+		live_cells++;
 	}
 	if (mod != 0 && local_board[lid - 1])// Left
 	{
-		live_cells++;	
+		live_cells++;
 	}
 	if (mod != size - 1 && local_board[lid + 1])// Right
 	{
-		live_cells++;	
+		live_cells++;
 	}
 	if (i - size>= 0 && mod != 0 && local_board[lid - size - 1])// Bottom left
 	{
-		live_cells++;	
+		live_cells++;
 	}
 	if (i - size >= 0 && local_board[lid - size])// Bottom
 	{
-		live_cells++;	
+		live_cells++;
 	}
 	if (i - size >= 0 && mod != size - 1 && local_board[lid - size + 1])// Bottom right
 	{
-		live_cells++;	
+		live_cells++;
 	}
 
 	int is_live = local_board[lid];
 	int result = is_live;
-	if (is_live == 1) // an einai alive
+	if (is_live == 1) // if it is alive
 	{
-		result = 2;//kanto dying
+		result = 2;//now it is dying
 	}
-	else if (is_live == 0 && live_cells == 2)//an einai off kai exei 2 akrivos on geitones
+	else if (is_live == 0 && live_cells == 2)//if it is OFF and has exactly 2 ON neighbors
 	{
-		result = 1;	//kanto on
+		result = 1;	//now it is on
 	}
-	else if (is_live == 2)//an einai dying
+	else if (is_live == 2)//if it is  dying
 	{
-		result = 0;//kanto off
+		result = 0;//now it is off
 	}
 	out[i] = result;
 
